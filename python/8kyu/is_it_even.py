e245c5d7c077a33f19f1e4b3b707dbefc128ffcb
Bayram Uysal <122267966+BayramUysalBey@users.noreply.github.com>
2025-02-21 14:37:39 +0300

    Update is_it_even.py

--- python/8kyu/is_it_even.py
+++ python/8kyu/is_it_even.py
@@ -1,13 +1,1 @@
-"""In this Kata we are passing a number (n) into a function.
 
-Your code will determine if the number passed is even (or not).
-
-The function needs to return either a true or false.
-
-Numbers may be positive or negative, integers or floats.
-
-Floats with decimal part non equal to zero are considered UNeven for this kata."""
-
-# My solution
-def is_even(n):
-    return abs(n) % 2 == 0 and isinstance(n, int) or isinstance(n, float) and abs(n) - int(abs(n)) == 0
