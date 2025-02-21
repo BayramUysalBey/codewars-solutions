917d693b874f26d6fa2a1c037c02213c9c28f483
BayramUysalBey <bayramuysal@windowslive.com>
2025-02-21 14:49:32 +0300

    is it even done in pthon

--- python/8kyu/is_it_even.py
+++ python/8kyu/is_it_even.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """In this Kata we are passing a number (n) into a function.
 
 Your code will determine if the number passed is even (or not).
@@ -10,20 +9,6 @@ Numbers may be positive or negative, integers or floats.
 Floats with decimal part non equal to zero are considered UNeven for this kata."""
 
 # My solution
-def is_even(n):
+def is_even(n): 
+    # your code here
     return abs(n) % 2 == 0 and isinstance(n, int) or isinstance(n, float) and abs(n) - int(abs(n)) == 0
-=======
-"""In this Kata we are passing a number (n) into a function.
-
-Your code will determine if the number passed is even (or not).
-
-The function needs to return either a true or false.
-
-Numbers may be positive or negative, integers or floats.
-
-Floats with decimal part non equal to zero are considered UNeven for this kata."""
-
-# My solution
-def is_even(n):
-    return abs(n) % 2 == 0 and isinstance(n, int) or isinstance(n, float) and abs(n) - int(abs(n)) == 0
->>>>>>> 35a8d465feeb4df6225d365cd57fdd2c6c3d9265
