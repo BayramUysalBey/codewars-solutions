a59e71cce72018780f2f542d19d222680ebcbdba
BayramUysalBey <bayramuysal@windowslive.com>
2025-03-25 15:19:08 +0300

    leonardo dicaprio and oscars done in python

---
+++ python/8kyu/leonardo_dicaprio_and_oscars.py
@@ -0,0 +1,14 @@
+"""You have to write a function that describe Leo:
+
+def leo(oscar):
+  pass
+if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was over 88 you should return "Leo got one already!"""
+
+# My solution
+def leo(oscar):
+    if oscar == 88: return  "Leo finally won the oscar! Leo is happy"
+    elif oscar == 86: return "Not even for Wolf of wallstreet?!"
+    elif oscar < 86 or oscar < 88: return "When will you give Leo an Oscar?"
+    elif oscar > 88: return "Leo got one already!"
