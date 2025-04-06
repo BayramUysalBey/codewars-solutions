8768083d8cc92458b0c5dfdc3d0c8ff6c2c1d9b9
BayramUysalBey <bayramuysal@windowslive.com>
2025-04-06 18:30:33 +0300

    drink about done in python

---
+++ python/8kyu/drink_about.py
@@ -0,0 +1,26 @@
+'''Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"'''
+
+# My solution
+def people_with_age_drink(age):
+    if age > 20: return 'drink whisky'
+    if age > 17: return 'drink beer'
+    if age > 13: return 'drink coke'
+    return 'drink toddy'
