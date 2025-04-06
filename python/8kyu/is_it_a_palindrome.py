70890e99eee1f785047b1f97ece6890279cfb273
BayramUysalBey <bayramuysal@windowslive.com>
2025-04-06 18:43:07 +0300

    is it a palindrome done in python

---
+++ python/8kyu/is_it_a_palindrome.py
@@ -0,0 +1,8 @@
+"""Write a function that checks if a given string (case insensitive) is a palindrome.
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar."""
+
+# My solution
+def is_palindrome(s):
+    s = s.lower()
+    return s == s[::-1]
