122ef9650eeebfbf550f0f9599fb89952ed51f3b
BayramUysalBey <bayramuysal@windowslive.com>
2025-05-28 11:04:13 +0300

    sort arrays-1 done in python.

---
+++ python/7kyu/sort_arrays_1.py
@@ -0,0 +1,10 @@
+"""Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order."""
+
+# My solution
+def sortme(names):
+	s_names = sorted(names)
+	return s_names
+
+# My updated solution
+def sortme(names):
+    return sorted(names)
