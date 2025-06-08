5eb6db78c2736abb1d0479fa8a0c2c2fe4f2de20
BayramUysalBey <bayramuysal@windowslive.com>
2025-06-08 11:41:21 +0300

    nickname generator done in python.

---
+++ python/7kyu/nickname_generator.py
@@ -0,0 +1,49 @@
+"""Nickname Generator
+
+Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+
+If the 3rd letter is a consonant, return the first 3 letters.
+
+nickname("Robert") //=> "Rob"
+nickname("Kimberly") //=> "Kim"
+nickname("Samantha") //=> "Sam"
+If the 3rd letter is a vowel, return the first 4 letters.
+
+nickname("Jeannie") //=> "Jean"
+nickname("Douglas") //=> "Doug"
+nickname("Gregory") //=> "Greg"
+If the string is less than 4 characters, return "Error: Name too short".
+
+Notes:
+
+Vowels are "aeiou", so discount the letter "y".
+Input will always be a string.
+Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+The input can be modified"""
+
+# My solution
+def nickname_generator(name):
+    if len(name) <= 3:
+        return "Error: Name too short"
+    letter = "aeiou"
+    char = name[2]
+    result = ""
+    
+    if char in letter:
+        result += name[:4]
+    else:
+        result += name[:3]
+    return result
+
+# My updated solution
+def nickname_generator(name):
+    if len(name) <= 3:
+        return "Error: Name too short"
+    
+    vowels = "aeiou"
+    third_char = name[2]
+    
+    if third_char in vowels:
+        return name[:4]
+    else:
+        return name[:3]
