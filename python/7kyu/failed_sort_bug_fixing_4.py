2ca29928137573759ade35eccf8668425453c8aa
BayramUysalBey <bayramuysal@windowslive.com>
2025-07-01 11:32:02 +0300

    'Failed Sort - Bug Fixing #4' done in python.

---
+++ python/7kyu/failed_sort_bug_fixing_4.py
@@ -0,0 +1,6 @@
+"""Failed Sort - Bug Fixing #4
+Oh no, Timmy's Sort doesn't seem to be working? Your task is to fix the sortArray function to sort all numbers in ascending order"""
+
+# My solution
+def sort_array(value):
+    return "".join(sorted(value,key=lambda a: -int(a), reverse =True))
