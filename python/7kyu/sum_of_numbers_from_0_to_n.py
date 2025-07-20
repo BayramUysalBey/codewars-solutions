8b656c79b4b25da764e74778600c70bf7b7af692
BayramUysalBey <bayramuysal@windowslive.com>
2025-07-20 11:36:18 +0300

    'Sum of numbers from 0 to N' done in python.

---
+++ python/7kyu/sum_of_numbers_from_0_to_n.py
@@ -0,0 +1,39 @@
+'''We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+Example:
+Input:
+
+> 6
+Output:
+
+0+1+2+3+4+5+6 = 21
+
+Input:
+
+> -15
+Output:
+
+-15<0
+
+Input:
+
+> 0
+Output:
+
+0=0'''
+
+# My solution
+def show_sequence(n):
+    if n == 0:
+        return "0=0"
+    elif n < 0:
+        return f"{n}<0"
+    res = 0
+    adding = ""
+    for i in range(0,n+1):
+        res += i
+        adding += str(i)
+        if i < n:
+            adding += "+"
+            
+    return f"{adding} = {res}"
