60e435af39641e642d78317e4cd10cc8ba2028a3
BayramUysalBey <bayramuysal@windowslive.com>
2025-07-22 11:50:28 +0300

    adding minor changes

--- sql/8kyu/multiply.sql
+++ sql/8kyu/multiply.sql
@@ -1,4 +1,4 @@
-"""This code does not execute properly. Try to figure out why."""
+-- This code does not execute properly. Try to figure out why.
 
 -- My solution
 SELECT (price*amount) AS total FROM items
