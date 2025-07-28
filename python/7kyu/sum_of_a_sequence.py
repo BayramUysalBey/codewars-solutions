b6a470cd50ac3669e7b308215879533f58e12952
BayramUysalBey <bayramuysal@windowslive.com>
2025-07-28 16:25:59 +0300

    'Sum of a sequence' done in python.

---
+++ python/7kyu/sum_of_a_sequence.py
@@ -0,0 +1,25 @@
+"""Your task is to write a function which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step.
+
+If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)"""
+
+# My solution
+def sequence_sum(begin_number, end_number, step):
+    if begin_number > end_number:
+        return 0
+    result = 0
+    for i in range(begin_number, end_number + 1, step):
+        result += i
+    return result
+
+# My updated solution
+def sequence_sum(start, end, step):
+    return sum(range(start, end+1, step))
