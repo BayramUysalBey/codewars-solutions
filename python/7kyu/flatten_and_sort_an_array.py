a07e736752aca07264f3b20c8d392eff051e59fe
BayramUysalBey <bayramuysal@windowslive.com>
2025-08-07 16:21:43 +0300

    'Flatten and sort an array' done in python.

---
+++ python/7kyu/flatten_and_sort_an_array.py
@@ -0,0 +1,23 @@
+"""Challenge:
+
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9]."""
+
+# My solution
+def flatten_and_sort(array):
+    if array == []:
+    	return []
+    result = []
+    for char in array:
+        for num in char:
+            result.append(num)
+    return sorted(result)
+
+# My updated solution
+def flatten_and_sort(array):
+    if array == []:
+    	return []
+    return sorted([num for char in array for num in char])
