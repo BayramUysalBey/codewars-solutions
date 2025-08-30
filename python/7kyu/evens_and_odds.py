91e368495b209718facce177a695eb4a0b69210f
BayramUysalBey <bayramuysal@windowslive.com>
2025-08-30 13:33:14 +0300

    'Evens and Odds' done in python.

---
+++ python/7kyu/evens_and_odds.py
@@ -0,0 +1,10 @@
+"""This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased."""
+
+# My solution
+def evens_and_odds(n):
+    if n % 2 == 0: return str(int(bin(n)[2:]))
+    else: return str(hex(n).split('x')[-1])
