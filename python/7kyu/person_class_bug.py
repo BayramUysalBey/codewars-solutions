f1ca4bf626ed77d91e61a3695fe7baf0227a4f05
BayramUysalBey <bayramuysal@windowslive.com>
2025-10-08 13:01:33 +0300

    'Person Class Bug' solved with python

---
+++ python/7kyu/person_class_bug.py
@@ -0,0 +1,21 @@
+"""The following code was thought to be working properly, however when the code tries to access the age of the person instance it fails.
+
+person = Person('Yukihiro', 'Matsumoto', 47)
+print(person.full_name)
+print(person.age)
+For this exercise you need to fix the code so that it works correctly.
+
+Note: the order of the person's full name is first name and last name."""
+
+# My solution
+class Person():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = f"{first_name} {last_name}"
+        self.age = age
+    def __str__(self):
+        return f"{person.first_name}  {person.last_name}  {person.age}"
+
+person = Person("John", "Doe", 30)
