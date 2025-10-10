d0d42bdb55c408dd79569c63ad7e0305ecf4954d
BayramUysalBey <bayramuysal@windowslive.com>
2025-10-10 11:27:02 +0300

    adding clean code solution

--- python/7kyu/descending_order.py
+++ python/7kyu/descending_order.py
@@ -8,9 +8,13 @@ Input: 145263 Output: 654321
 Input: 123456789 Output: 987654321"""
 
 # My solution
+def descending_order(num):
+    n_list = list(str(num))
+    n_list.sort(reverse=True)
+    return int(''.join(n_list))
+
+# My updated solution
 def Descending_Order(num):
 	return int("".join(sorted(str(num), reverse=True)))
-
-
 
 
