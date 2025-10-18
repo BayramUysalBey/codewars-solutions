f32666a0d2e0023f6e19b2fdc2e118d961a15866
BayramUysalBey <bayramuysal@windowslive.com>
2025-10-18 13:01:25 +0300

    another solution has been added

--- python/7kyu/odd_or_even.py
+++ python/7kyu/odd_or_even.py
@@ -17,4 +17,11 @@ Have fun!"""
 
 # My solution
 def odd_or_even(arr):
+    if sum(arr)%2==0:
+        return 'even'
+    else:
+        return 'odd'
+    
+# My updated solution
+def odd_or_even(arr):
     return "even" if sum(arr) % 2 == 0 else "odd"
