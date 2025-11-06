055ad1564e8dec4735a7e14d657f556e8962df1f
BayramUysalBey <bayramuysal@windowslive.com>
2025-11-06 11:30:27 +0300

    adding diffrent solution way

--- python/7kyu/sorted_yes_no_how.py
+++ python/7kyu/sorted_yes_no_how.py
@@ -7,6 +7,19 @@ You can assume the array will always be valid, and there will always be one corr
 
 # My solution
 def is_sorted_and_how(arr):
+	s_arr = sorted(arr, reverse=True)
+	s_arr1 = sorted(arr, reverse=False)
+
+
+	if arr == s_arr:
+		return "yes, descending"
+	elif arr == s_arr1:
+		return "yes, ascending"
+	else:
+		return "no"
+
+# My updated solution
+def is_sorted_and_how(arr):
 	if arr == sorted(arr, reverse=True):
 		return "yes, descending"
 	elif arr == sorted(arr, reverse=False):
