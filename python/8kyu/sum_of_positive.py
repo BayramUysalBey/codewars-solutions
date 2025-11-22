07939f06a120961d03d77a3e66749ef5b24eebdf
BayramUysalBey <bayramuysal@windowslive.com>
2025-11-22 12:22:37 +0300

    adding diffrent solution way

--- python/8kyu/sum_of_positive.py
+++ python/8kyu/sum_of_positive.py
@@ -7,4 +7,12 @@ If there is nothing to sum, the sum is default to 0."""
 
 # My solution
 def positive_sum(arr):
+    result = 0
+    for num in arr:
+        if num > 0:
+            result += num
+    return result
+
+# My updated solution
+def positive_sum(arr):
 	return sum(num for num in arr if num > 0)
