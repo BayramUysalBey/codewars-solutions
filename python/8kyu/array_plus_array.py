77d2b751f5fd7c0ff906c6abb687ff25eb8fadf5
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-01 12:27:28 +0300

    adding diffrent solution way

--- python/8kyu/array_plus_array.py
+++ python/8kyu/array_plus_array.py
@@ -4,4 +4,10 @@ P.S. Each array includes only integer numbers. Output is a number too."""
 
 # My solution
 def array_plus_array(arr1,arr2):
+    r1 = sum(arr1)
+    r2 = sum(arr2)
+    return r1 + r2
+
+# My updated solution
+def array_plus_array(arr1,arr2):
     return sum (arr1 + arr2)
