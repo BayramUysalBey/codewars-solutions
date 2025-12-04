739df800291425b8262d36509dc6999f392ee25b
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-04 12:05:26 +0300

    adding different solution way

--- python/8kyu/invert_values.py
+++ python/8kyu/invert_values.py
@@ -7,5 +7,13 @@ You can assume that all values are integers. Do not mutate the input array."""
 
 # My solution
 def invert(lst):
+    res = []
+    for ls in lst:
+        if ls in lst:
+            res.append(-1 * ls)
+    return res
+
+# My updated solution
+def invert(lst):
     if lst is []: return []
     return [-1 * num for num in lst]
