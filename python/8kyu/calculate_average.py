96cb985d93130ce16944fa0f1bf592fe2c389065
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-05 12:24:06 +0300

    adding different solution way

--- python/8kyu/calculate_average.py
+++ python/8kyu/calculate_average.py
@@ -4,5 +4,12 @@ Note: Empty arrays should return 0."""
 
 # My solution
 def find_average(numbers):
+	if sum(numbers) > 0:
+		return sum(numbers) / len(numbers)
+	else:
+		return 0
+
+# My updated solution
+def find_average(numbers):
     if numbers == []: return 0
     return sum(numbers) / len(numbers)
