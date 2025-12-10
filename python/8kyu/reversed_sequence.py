77c4d504fcdb42328f75071cb8d9319bef823060
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-10 11:32:01 +0300

    adding different solution way

--- python/8kyu/reversed_sequence.py
+++ python/8kyu/reversed_sequence.py
@@ -4,4 +4,11 @@ Example : n=5 --> [5,4,3,2,1]"""
 
 # My solution
 def reverse_seq(n):
+    res = []
+    for n in range(1, n + 1):
+        res.append(n)
+    return res[::-1]
+
+# My updated solution
+def reverse_seq(n):
     return list(range(n, 0, -1))
