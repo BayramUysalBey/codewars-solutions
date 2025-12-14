60c63f127b9d60183245c75bfaeffa61c233fad0
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-14 11:53:47 +0300

    adding other solution

--- python/8kyu/switch_it_up.py
+++ python/8kyu/switch_it_up.py
@@ -8,4 +8,27 @@ If your language supports it, try using a switch statement."""
 
 # My solution
 def switch_it_up(number):
+    if number == 0:
+        return "Zero"
+    elif number == 1:
+        return "One"
+    elif number == 2:
+        return "Two"
+    elif number == 3:
+        return "Three"
+    elif number == 4:
+        return "Four"
+    elif number == 5:
+        return "Five"
+    elif number == 6:
+        return "Six"
+    elif number == 7:
+        return "Seven"
+    elif number == 8:
+        return "Eight"
+    elif number == 9:
+        return "Nine"
+
+# My updated solution
+def switch_it_up(number):
     return 'Zero One Two Three Four Five Six Seven Eight Nine'.split()[number]
