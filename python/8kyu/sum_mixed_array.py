cefe476514d1c99cea6591688193d4d2a9f7ccc3
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-20 14:44:49 +0300

    added other solution way

--- python/8kyu/sum_mixed_array.py
+++ python/8kyu/sum_mixed_array.py
@@ -4,4 +4,14 @@ Return your answer as a number."""
 
 # My solution
 def sum_mix(arr):
+	res = []
+	for ey in arr:
+		if type(ey) == str:
+			res.append(int(ey))
+		elif type(ey) == int:
+			res.append(ey)
+	return sum(res)
+
+# My updated solution
+def sum_mix(arr):
     return sum(map(int, arr))
