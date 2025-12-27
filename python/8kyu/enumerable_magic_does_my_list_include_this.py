46708f4af2bfe95f29af7c9ab7b6811c5e9ad8c3
BayramUysalBey <bayramuysal@windowslive.com>
2025-12-27 14:57:29 +0300

    added different solution way

--- python/8kyu/enumerable_magic_does_my_list_include_this.py
+++ python/8kyu/enumerable_magic_does_my_list_include_this.py
@@ -1,5 +1,12 @@
 """Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false."""
 
 # My solution
+def include(arr, item):
+    if item in arr:
+        return True
+    else:
+        return False
+
+# My updated solution
 def include(arr,item):
     return item in arr
