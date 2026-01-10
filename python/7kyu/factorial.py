01becf6ffe2b06fe0c76fe769c9becd7cce95d0d
BayramUysalBey <bayramuysal@windowslive.com>
2026-01-10 13:00:12 +0300

    another solution has been added

--- python/7kyu/factorial.py
+++ python/7kyu/factorial.py
@@ -5,4 +5,16 @@ https://en.wikipedia.org/wiki/Factorial"""
 # My solution
 import math
 def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0 or n > 12:
+        raise ValueError
+    else:
+        return math.factorial(n)
+
+# My updated solution
+import math
+def factorial(n):
+    if n < 0 or n > 12:
+        raise ValueError
     return math.factorial(n)
