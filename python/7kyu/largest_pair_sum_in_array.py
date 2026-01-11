1a4ac9753e4adebe34509e5fc536800222745161
BayramUysalBey <bayramuysal@windowslive.com>
2026-01-11 11:45:06 +0300

    another solution has been added

--- python/7kyu/largest_pair_sum_in_array.py
+++ python/7kyu/largest_pair_sum_in_array.py
@@ -10,3 +10,7 @@ Input sequence contains minimum two elements and every element is an integer."""
 def largest_pair_sum(numbers): 
     s_numbers = sorted(numbers, reverse = True)
     return s_numbers[0] + s_numbers[1]
+
+# My updated solution
+def largest_pair_sum(numbers): 
+    return sum(sorted(numbers)[-2:])
