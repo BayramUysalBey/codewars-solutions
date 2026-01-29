88b48495f04e13060b31d59d3d81aa91ef7b2b08
BayramUysalBey <bayramuysal@windowslive.com>
2026-01-29 11:24:14 +0300

    'grouped by commas' done in python

---
+++ python/6kyu/grouped_by_commas.py
@@ -0,0 +1,25 @@
+'''Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+Assume: 0 <= n <= 2147483647
+
+Examples
+       1  ->           "1"
+      10  ->          "10"
+     100  ->         "100"
+    1000  ->       "1,000"
+   10000  ->      "10,000"
+  100000  ->     "100,000"
+ 1000000  ->   "1,000,000"
+35235235  ->  "35,235,235" '''
+
+# My solution
+def group_by_commas(n):
+    s = str(n)
+    if len(s) <= 3:
+        return s
+    else:
+        rev = s[::-1]
+        groups = [rev[i:i+3] for i in range(0, len(rev), 3)]
+        joined = ','.join(groups)
+        result = joined[::-1]
+        return result
