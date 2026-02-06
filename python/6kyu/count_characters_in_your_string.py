cef88c909be240cadf4147afc029aabfc720c31a
BayramUysalBey <bayramuysal@windowslive.com>
2026-02-06 13:44:52 +0300

    Implement character counting function to tally occurrences in a string

---
+++ python/6kyu/count_characters_in_your_string.py
@@ -0,0 +1,12 @@
+"""The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}."""
+
+# My solution
+def count(s):
+    if s == "":
+        return {}
+    result = {}
+    for char in s:
+        result[char] = result.get(char, 0) + 1
+    return result
